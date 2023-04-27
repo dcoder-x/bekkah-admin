977c22dfba3e06f581cbccb463521283d491beb2
dcoder-x <ayomikunfaluyi@gmail.com>
2023-04-27 13:08:21 +0100

    added category and subcategory

--- src/components/ProductUploadForm.jsx
+++ src/components/ProductUploadForm.jsx
@@ -295,7 +295,7 @@ function ProductUploadForm() {
             OnSelectCategory={(category) => handleCategorySelect(category)}
 
           />
-          <BrandSelect />
+          {/* <BrandSelect /> */}
           <div className="mb-4">
             <label className="block text-gray-700 font-bold mb-2">
               Product Variants
