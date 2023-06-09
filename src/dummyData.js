de1227b388a0342ba201caa8e2a65be5f6cdabe4
dcoder-x <ayomikunfaluyi@gmail.com>
2023-06-09 12:00:50 +0100

    update dummy.js

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,51 +1,51 @@
 export const userData = [
     {
       name: "Jan",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Feb",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Mar",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Apr",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "May",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Jun",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Jul",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Agu",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Sep",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Oct",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Nov",
-      "Active User": 0,
+      "totalSale": 0,
     },
     {
       name: "Dec",
-      "Active User": 0,
+      "totalSale": 0,
     },
   ];
 
